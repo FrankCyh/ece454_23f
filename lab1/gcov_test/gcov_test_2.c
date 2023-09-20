215c62656d2fce9f843fa2279f160d7adf9a1645
Yuhe Chen <yuhe.chen@mail.utoronto.ca>
2023-09-20 17:56:05 -0400

    start lab1

---
+++ lab1/gcov_test/gcov_test_2.c
@@ -0,0 +1,51 @@
+// gprof_test.c
+#include<stdio.h>
+
+static void func2(void);
+
+void
+func1_child(void)
+{
+	printf("\nInside func1_child()\n");
+	unsigned int i = 0;
+
+	for (; i < 0xfffff777; i++);
+
+	return;
+}
+
+void
+func1(void)
+{
+	printf("\nInside func1()\n");
+	unsigned int i = 0;
+
+	for (; i < 0xffffffff; i++);
+	func1_child();
+
+	func2();
+	return;
+}
+
+static void
+func2(void)
+{
+	printf("\nInside func2()\n");
+	unsigned int i = 0;
+
+	for (; i < 0xfffff000; i++);
+	return;
+}
+
+int
+main(void)
+{
+	printf("\nInside main()\n");
+	int i = 0;
+
+	for (; i < 0xfffffff; i++);
+	func1();
+	func2();
+
+	return 0;
+}
